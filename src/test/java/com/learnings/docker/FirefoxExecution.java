f90a83a859286910ea695dcf3b81a0dcbb3eb330
Ranjit.Biswal <ranjitkumar.it@gmail.com>
2021-08-12 11:27:39 +0530

    Learnings only

---
+++ src/test/java/com/learnings/docker/FirefoxExecution.java
@@ -0,0 +1,23 @@
+package com.learnings.docker;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class FirefoxExecution {
+	@Test
+	public void Firefox() throws MalformedURLException {
+
+		DesiredCapabilities cap = DesiredCapabilities.edge();
+
+		URL url = new URL("http://localhost:4444/wd/hub");
+
+		RemoteWebDriver driver = new RemoteWebDriver(url, cap);
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+	}
+
+}
