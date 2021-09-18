6c4f2384e50e3b4cba0b3415129d8f2818f8d679
Ranjit.Biswal <ranjitkumar.it@gmail.com>
2021-09-18 21:23:16 +0530

    some

--- src/test/java/com/learnings/selenium/MiniDriver.java
+++ src/test/java/com/learnings/selenium/MiniDriver.java
@@ -1,7 +1,9 @@
 package com.learnings.selenium;
 
 import java.net.MalformedURLException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -19,20 +21,29 @@ public class MiniDriver {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://qa.smartcmobile.net/SCM_10.0_S-SCP");
-		
+
 		WebElement footerElement = driver.findElement(By.cssSelector(".container ul.group-links"));
-		List<WebElement> footerlinks =footerElement.findElements(By.tagName("a"));
-		
-		for(WebElement footerlink : footerlinks)
-		{
+		List<WebElement> footerlinks = footerElement.findElements(By.tagName("a"));
+
+		for (WebElement footerlink : footerlinks) {
 			System.out.println(footerlink.getText());
-			
-			String clickbyKeyboard=Keys.chord(Keys.CONTROL, Keys.ENTER);
+
+			String clickbyKeyboard = Keys.chord(Keys.CONTROL, Keys.ENTER);
 			footerlink.sendKeys(clickbyKeyboard);
+			Thread.sleep(2000);
+
 		}
-		Thread.sleep(2000);
+
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		while (it.hasNext()) {
+
+			String windowTitle = driver.switchTo().window(it.next()).getTitle();
+			System.out.println(windowTitle);
+
+		}
+
 		driver.quit();
-		
 
 	}
 
