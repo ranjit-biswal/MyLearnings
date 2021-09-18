728402e0e671f2fed13dae228680ab40460be7fd
Ranjit.Biswal <ranjitkumar.it@gmail.com>
2021-09-18 12:21:08 +0530

    new files

--- src/test/java/com/learnings/Grid/WebdriverGrid.java
+++ src/test/java/com/learnings/Grid/WebdriverGrid.java
@@ -1,10 +1,24 @@
 package com.learnings.Grid;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class WebdriverGrid {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 
+		
+		DesiredCapabilities dc= new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		dc.setPlatform(Platform.WINDOWS);
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.181.146:4444/wd/hub"), dc);
+		driver.get("https://www.google.com/");
 	}
 
 }
---
+++ src/test/java/com/learnings/selenium/MiniDriver.java
@@ -0,0 +1,39 @@
+package com.learnings.selenium;
+
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MiniDriver {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://qa.smartcmobile.net/SCM_10.0_S-SCP");
+		
+		WebElement footerElement = driver.findElement(By.cssSelector(".container ul.group-links"));
+		List<WebElement> footerlinks =footerElement.findElements(By.tagName("a"));
+		
+		for(WebElement footerlink : footerlinks)
+		{
+			System.out.println(footerlink.getText());
+			
+			String clickbyKeyboard=Keys.chord(Keys.CONTROL, Keys.ENTER);
+			footerlink.sendKeys(clickbyKeyboard);
+		}
+		Thread.sleep(2000);
+		driver.quit();
+		
+
+	}
+
+}
