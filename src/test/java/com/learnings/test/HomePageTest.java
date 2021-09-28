eba9337178ebd98096a4eec8538ed50b24b6cf05
Ranjit.Biswal <ranjitkumar.it@gmail.com>
2021-09-28 18:41:08 +0530

    new

--- src/test/java/com/learnings/test/HomePageTest.java
+++ src/test/java/com/learnings/test/HomePageTest.java
@@ -11,16 +11,17 @@ import com.learnings.page.SCMHomePage;
 public class HomePageTest {
 
 	@Test
-	public void Login() {
+	public void validateLogin() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://qa.smartcmobile.net/SCM_10.0_S-SCP/default.aspx");
 		SCMHomePage homePage = new SCMHomePage(driver);
-		homePage.txtBoxUsername().sendKeys("hello");
-		homePage.txtBoxPassword().sendKeys("nope");
+		homePage.txtBoxUsername().sendKeys("business_-&+commercial");
+		homePage.txtBoxPassword().sendKeys("Demo@123");
 		homePage.btnLogin().click();
+		Thread.sleep(10000);
 		driver.close();
 	}
 
