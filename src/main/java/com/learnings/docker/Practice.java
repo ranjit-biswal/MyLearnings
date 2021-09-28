9a27982434d89b18f087f24183b025492477779e
Ranjit.Biswal <ranjitkumar.it@gmail.com>
2021-09-28 15:54:37 +0530

    new

---
+++ src/main/java/com/learnings/docker/Practice.java
@@ -0,0 +1,17 @@
+package com.learnings.docker;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class Practice 
+{
+	
+	@Test
+    public void helloWorld()
+    {
+    	System.out.println("hello");
+    }
+}
---
+++ src/test/java/com/learnings/testng/MacValidations.java
@@ -0,0 +1,39 @@
+package com.learnings.testng;
+
+import org.testng.annotations.Test;
+
+public class MacValidations {
+
+	@Test
+	public void showWeb() {
+
+		System.out.println("Web Show");
+
+	}
+	
+	
+	
+	@Test
+	public void showWeb1() {
+
+		System.out.println("Web Show");
+
+	}
+
+	@Test
+	public void showMob() {
+
+		System.out.println("Web Show");
+
+	}
+	
+	@Test
+	public void showMob1() {
+
+		System.out.println("Web Show");
+
+	}
+
+
+
+}
